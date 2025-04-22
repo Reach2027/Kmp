93569e0c2036a62b8312b820c240b95979223ac4
Reach <reach2027@outlook.com>
2025-04-22 09:36:33 +0800

    Modify spotless config

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Reach Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 initscript {
     val spotlessVersion = "7.0.3"
 
@@ -12,23 +27,23 @@ initscript {
 }
 
 rootProject {
-    subprojects {
+    allprojects {
         apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
         extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
             kotlin {
                 target("**/*.kt")
-                targetExclude("**/build/**/*.kt")
+                targetExclude("**/build/**/*.kt", "**/spotless/**")
                 licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
             }
             format("kts") {
                 target("**/*.kts")
-                targetExclude("**/build/**/*.kts")
+                targetExclude("**/build/**/*.kts", "**/spotless/**")
                 // Look for the first line that doesn't have a block comment (assumed to be the license)
                 licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
             }
             format("xml") {
                 target("**/*.xml")
-                targetExclude("**/build/**/*.xml")
+                targetExclude("**/build/**/*.xml", "**/spotless/**")
                 // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
                 licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
             }
